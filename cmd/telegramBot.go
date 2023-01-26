f1bd8f3354bf628ccf60b627a51a3b408c886f0a
Stefan Heiden <stefanheiden@gmail.com>
2023-01-26 17:39:24 +0100

    cleaning

--- cmd/telegramBot.go
+++ cmd/telegramBot.go
@@ -17,24 +17,11 @@ func StartBot() error {
 
 	bot.Debug = true
 
-	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
-	// to make sure Telegram knows we've handled previous values and we don't
-	// need them repeated.
 	updateConfig := tgbotapi.NewUpdate(0)
-
-	// Tell Telegram we should wait up to 30 seconds on each request for an
-	// update. This way we can get information just as quickly as making many
-	// frequent requests without having to send nearly as many.
 	updateConfig.Timeout = 30
-
-	// Start polling Telegram for updates.
 	updates := bot.GetUpdatesChan(updateConfig)
 
-	// Let's go through each update that we're getting from Telegram.
 	for update := range updates {
-		// Telegram can send many types of updates depending on what your Bot
-		// is up to. We only want to look at messages for now, so we can
-		// discard any other updates.
 		if update.Message == nil {
 			continue
 		}
